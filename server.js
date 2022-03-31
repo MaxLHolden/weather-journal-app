63182fdaee1e3232e0b8d7fe4b745b0b85f2f125
Max Holden <malxholden@gmail.com>
2022-03-31 13:57:31 -0500

    updates server and app.js

--- server.js
+++ server.js
@@ -2,18 +2,44 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+function listening() {
+    console.log("Started on Port: + ${port}");
+}
+const server = app.listen(port, listening);
+
+// Post Route
+app.post('/add', addData);
+
+function addData(req, res) {
+  projectData['date'] = req.body.date;
+  projectData['temp'] = req.body.temp;
+  projectData['content'] = req.body.content;
+  res.send(projectData);
+}
+
+//get route
+app.get('/all', getData);
+
+function getData(req, res) {
+  res.send(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,13 @@
+const { appendFile } = require("fs");
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip="
+
 /* Global Variables */
+const apiKey = "f8c5f2aecdaf9cd29865690ea6558781&units=imperial";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+async function GetWeather(){
+    await fetch(baseURL+zip+"&appid="+key);
+}
