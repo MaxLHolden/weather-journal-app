dc75adaa8772c68f5893258a3870bde52bbfc685
Max Holden <malxholden@gmail.com>
2022-04-11 18:06:53 -0500

    updated date

--- website/app.js
+++ website/app.js
@@ -6,7 +6,7 @@ const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 document.getElementById('generate').addEventListener('click', generateOnClick);
 
